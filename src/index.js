567fc16768902e4d4078bf39bb6e0f5d14788cdc
Dias Samarkanov <77679797+Diassamarkanov98@users.noreply.github.com>
2021-03-15 18:51:35 +0600

    feat: complete human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function toReadable (number) {
-  
-}
+    var numString = number.toString();
+    var onesHundred = +(numString.substring(1));
+    var ones = ['', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen'];
+    var tens = ['', '', ' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety'];
+    var hundred = ' hundred';
+    var str = '';    
+
+    if (number == 0) {
+        return 'zero';
+    }
+
+    if (number < 20) {
+      return ones[number].trim(); 
+    }
+
+    if (numStr.length == 2) {
+    str += tens[+(numString[0])];
+    str += ones[+(numString[1])];
+      return str.trim();
+    }
+    
+    str =  ones[+(numString[0])] + hundred;
+    if (onesHundred<20){
+      str += ones[onesHundred];
+      return str.trim(); 
+    } else {
+      str += tens[+(numString[1])];
+      str += ones[+(numString[2])];
+      return str.trim();
+    }
+
+}   
